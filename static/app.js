d2b76895cef6a8bae8a379c25afe97819c1ca88b
Terence Huynh <me@terencehuynh.com>
2015-09-11 22:21:12 +1000

    Add terms, changed font, other stuff...

--- static/app.js
+++ static/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+  var now = new Date();
+  var launch = new Date(2015, 09, 14, 12, 0, 0);
+  var close = new Date(2015, 09, 21, 12, 0, 0);
+
+  if (now < launch) {
+    $(".message").removeClass("hidden").html("<h2>You're here too early!</h2><p>It doesn't begin until 12:00pm September 14.</p>");
+  } else if (now > close) {
+    $(".message").removeClass("hidden").html("<h2>Too late!</h2><p>The game has ended.</p>");
+  } else {
+    $("#form").removeClass("hidden");
+  }
+
   $("#wired-btn").click(function() {
     $("#wired-head").toggleClass("hide");
   });
@@ -7,21 +19,19 @@ $(document).ready(function() {
   $("#form").submit(function() {
 
     console.log("Posting... " + $("#code").val());
-
     console.log(JSON.stringify({code:"test"}));
 
     code = $("#code").val();
-	sid = $("#sid").val();
+    sid = $("#sid").val();
     if (code == "" && sid == "") {
       mark_error("Please enter a code and a student ID.");
-	} else if (code == ""){
-	  mark_error("Please enter a code");
-	} else if (sid == "") {
-	  mark_error("Please enter a student ID.");
-	} else {
-    	$.ajax({
-
-    		type: "POST",
+    } else if (code == ""){
+      mark_error("Please enter a code");
+    } else if (sid == "") {
+      mark_error("Please enter a student ID.");
+    } else {
+      $.ajax({
+        type: "POST",
     		url: "/get",
     		data: JSON.stringify({code: code, student_id: sid}),
     		contentType: 'application/json;charset=UTF-8',
@@ -33,9 +43,7 @@ $(document).ready(function() {
     			}
     			console.log(data.status);
     		}
-
     	});
-
     }
 
     event.preventDefault();
